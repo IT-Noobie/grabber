e9efda663f52e3a9e19d92f28b6f8a5183e83eaa
Héctor Ruiz <ruac.hector@gmail.com>
2022-11-04 01:46:00 +0100

    Added subcommand list

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use std::process::exit;
+
 use clap::{Parser, Subcommand};
 
-mod setup;
+mod list;
 mod new;
+mod setup;
 
 #[derive(Parser)]
 #[clap(about, version, author)]
@@ -21,13 +24,16 @@ enum Commands {
         /// Client to add the repository
         client: String,
     },
-	/// List client platforms and repositories
-	List {
-		#[clap(short, long)]
-		/// Name of the client to list
-		client: String,
-	},
-	/// Adds a new Client
+    /// List client platforms and repositories
+    List {
+        #[clap(short, long)]
+        /// Name of the client to list
+        client: Option<String>,
+        #[clap(short, long)]
+        /// Name of the platform key alias
+        platform: Option<String>,
+    },
+    /// Adds a new Client
     New {
         #[clap(short, long)]
         /// Name of the client to add
@@ -46,15 +52,49 @@ fn main() {
                 repo, client
             )
         }
-        Commands::List { client } => {
-            println!("Platforms of client: {}", client)
+        Commands::List { client, platform } => {
+            if client.is_some() && platform.is_some() {
+                match list::client_platform_repositories(
+                    &client.to_owned().unwrap(),
+                    &platform.to_owned().unwrap(),
+                ) {
+                    Ok(_) => exit(0),
+                    Err(err) => {
+                        eprintln!("ERROR: {}", err);
+                        exit(1);
+                    }
+                };
+            };
+            if client.is_some() && platform.is_none() {
+                match list::client_platform(&client.to_owned().unwrap()) {
+                    Ok(_) => exit(0),
+                    Err(err) => {
+                        eprintln!("ERROR: {}", err);
+                        exit(1);
+                    }
+                }
+            }
+            if client.is_none() && platform.is_some() {
+                match list::platform_key_alias_config(&platform.to_owned().unwrap()) {
+                    Ok(_) => exit(0),
+                    Err(err) => {
+                        eprintln!("ERROR: {}", err);
+                        exit(1);
+                    }
+                };
+            };
+            match list::platforms() {
+                Ok(_) => (),
+                Err(err) => {
+                    eprintln!("ERROR: {}", err);
+                    exit(1);
+                }
+            };
         }
         Commands::New { client } => {
             println!("New client {:?} will be configured", client);
             new::new(client)
         }
-        Commands::Setup => {
-            setup::setup()
-        }
+        Commands::Setup => setup::setup(),
     }
 }
